4a1c4f05a123237fba4385d6ba1e17d8e0224621
Brad Jenkins <jenkins.j.brad@gmail.com>
2026-02-26 16:00:30 -0500

    Untrack lite-xl session

--- .config/lite-xl/session.lua
+++
@@ -1,1 +0,0 @@
-return {recents={[3]="/home/brad/.dotfiles/.config/lite-xl",[1]="/home/brad/workspace",[2]="/home/brad",[4]="/home/brad/.config/lite-xl"}, window={[1]=1908,[2]=999,[3]=6,[4]=27,["n"]=4}, window_mode="maximized", previous_find={[1]="scroll"}, previous_replace={}}
